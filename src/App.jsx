81a23b6130fbb8676913b80dadfe23efaa131919
Fede <federicomorella@hotmail.com>
2023-04-19 21:25:23 -0300

    Add wouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Login from './Components/Login'
 import Logout from './Components/Logout'
 import Register from './Components/Register'
@@ -8,11 +8,21 @@ import { useUser } from './hooks/useUser'
 import { WSTest } from './Components/WSTest'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar,Button,Container,Nav,Stack} from 'react-bootstrap'
-
+import {Link,Route, useLocation} from 'wouter'
 
 function App() {
 
   const {user,socket,login,logout,register,updateUser}=useUser()
+  const [location,setLocation]=useLocation()
+  useEffect(()=>{
+    if(user?.username)
+      setLocation('/app',{socket:socket})
+    else
+      setLocation('/',{socket:socket})
+
+  },[user]
+  )
+
   console.log('app:',user)
   return (
     <Container bg="dark" fluid className="App">
@@ -34,7 +44,15 @@ function App() {
       </Navbar >
     
 
-      {user && user.accessToken?<WSTest socket={socket}/>:<></>}
+      {/* {user && user.accessToken?<WSTest socket={socket}/>:<></>} */}
+
+      <Route path="/">Welcome to my SocketIO sample project</Route>
+      <Route path="/user/:name">{(params) => <div>Hello, {params.name}!</div>}</Route>
+      <Route path="/app">
+        <WSTest socket={socket}/>
+      </Route>
+   
+
     </Container>
   )
 }
